2cf6076d6d4456f920efd76a84cf6f7a1a3ad17d
adrixop95 <adrixop95@me.com>
2019-03-12 15:13:06 +0100

    alpha 1

---
+++ src/Panning and Zooming Android/Assets/Scripts/Control_Camera.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Control_Camera : MonoBehaviour {
+
+
+    private Vector3     touchStart;
+    public  float       zoomOutMin = 1;
+    public  float       zoomSpeed   =   8f;
+    public  float       zoomOutMax = 10;
+    public  Vector3[]   moveBorders =   new Vector3[2];
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (PinchToZoom()) { return; };
+        MoveCamera();
+        OnTouchObject();
+    }
+
+    // ##########################################################################################
+    private Vector3 TouchPoint() {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    // ------------------------------------------------------------------------------------------
+    public RaycastHit2D[] Touch2D( TouchPhase touchPhase ) {
+        List<RaycastHit2D>  result  =   new List<RaycastHit2D>();
+        for ( var i = 0; i < Input.touchCount; ++i ) {
+            if ( Input.GetTouch(i).phase == touchPhase ) {
+                Vector3         hitPoint    =   Camera.main.ScreenToWorldPoint( Input.GetTouch(i).position );
+                RaycastHit2D    hitInfo     =   Physics2D.Raycast( hitPoint, Vector2.zero );
+                result.Add( hitInfo );
+            }
+        }
+        return result.ToArray();
+    }
+
+
+
+    // ------------------------------------------------------------------------------------------
+    public void OnTouchObject() {
+        foreach( var touch in Touch2D( TouchPhase.Began ) ) {
+            if ( touch ) {
+                GameObject hit = touch.transform.gameObject;
+                if ( hit.tag == "Hittable" ) {
+                    Destroy(hit);
+                } else if ( hit.tag == "HitEnd") {
+                    SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+                }
+            }
+        }
+    }
+
+    // ##########################################################################################
+    private void MoveCamera() {
+        //  Touch Start
+        if (Input.GetMouseButtonDown(0)) {
+            touchStart = TouchPoint();
+            GameObject.Find("Text").GetComponent<Text>().text = "";
+        }
+        //  Touch End
+        if (Input.GetMouseButton(0)) {
+            Vector3 direction   =   touchStart - TouchPoint();
+            Camera.main.transform.position = Tools.ClampMove(
+                Camera.main.transform.position, direction, moveBorders
+            );
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------
+    private bool PinchToZoom() {
+        if ( Input.touchCount == 2 ) {
+            Touch   touchOne            =   Input.GetTouch(0);
+            Touch   touchTwo            =   Input.GetTouch(1);
+            Vector2 touchOnePrevPos     =   touchOne.position - touchOne.deltaPosition;
+            Vector2 touchTwoPrevPos     =   touchTwo.position - touchTwo.deltaPosition;
+            float   previousMagnitude   =   (touchOnePrevPos - touchTwoPrevPos).magnitude;
+            float   currentMagnitude    =   (touchOne.position - touchTwo.position).magnitude;
+            float   difference          =   currentMagnitude - previousMagnitude;
+            Zoom( difference );
+            return true;
+        }
+        return false;
+    }
+
+    // ------------------------------------------------------------------------------------------
+    void Zoom( float increment ) {
+        float   diffrence   =   zoomSpeed * Time.deltaTime * (increment < 0 ? -1 : 1);
+        float   newSize     =   Mathf.Clamp( Camera.main.orthographicSize - diffrence, zoomOutMin, zoomOutMax );
+        Camera.main.orthographicSize    =   newSize;
+    }
+
+    // ##########################################################################################
+}
---
+++ src/Panning and Zooming Android/Assets/Scripts/Spawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public  GameObject[]    prefabs =   new GameObject[2];
+    public  Vector3[]       points  =   new Vector3[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnSanta( new Vector3( 8, 6, 0 ), new Vector3( -8, -6, 0 ) );
+        SpawnTrees( 20, new Vector3( 10, 8, 0 ), new Vector3( -10, -8, 0  ) );
+        SpawnRock(9, new Vector3(10, 8, 0), new Vector3(-10, -8, 0));
+    }
+
+    private void SpawnSanta( Vector3 tr, Vector3 bl ) {
+        GameObject  santa   =   Instantiate( prefabs[0], transform );
+        float       x       =   Random.Range(bl.x, tr.x);
+        float       y       =   Random.Range(bl.y, tr.y);
+
+
+        santa.transform.position    =   new Vector3(
+            x, y, 1
+        );
+
+        GameObject stone    =   Instantiate( prefabs[1], transform );
+        stone.transform.position = new Vector3(
+            points[1].x + x,
+            points[1].y + y,
+            0
+        );
+    }
+
+    private void SpawnTrees( int n, Vector3 tr, Vector3 bl ) {
+        for ( int i = 0; i < n; i++ ) {
+            GameObject  prefab  =   prefabs[ Random.Range( 2, 4 ) ];
+            GameObject  tree    =   Instantiate( prefab, transform );
+            float       x       =   Random.Range(bl.x, tr.x);
+            float       y       =   Random.Range(bl.y, tr.y);
+            tree.transform.position = new Vector3( x, y, -1 );
+        }
+
+    }
+
+    private void SpawnRock( int n, Vector3 tr, Vector3 bl ) {
+        for ( int i = 0; i < n; i++ ) {
+            GameObject  rock    =   Instantiate( prefabs[1], transform );
+            float       x       =   Random.Range(bl.x, tr.x);
+            float       y       =   Random.Range(bl.y, tr.y);
+            rock.transform.position = new Vector3( x, y, 1 );
+        }
+
+    }
+}
